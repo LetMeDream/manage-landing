87b833f3bd7d4ca52735069df775259834963a77
Let <raulalfonzo66@gmail.com>
2022-05-17 22:25:52 -0400

    Move the addEventListener into createProgressBar(), so we attack those listeners again when resizing

--- js/carousel.js
+++ js/carousel.js
@@ -40,15 +40,47 @@ const createProgressBar = () => {
         
     }
 
+    /* Event listeners for our progress buttons */
+    for (let i = 0; i < progressBtns.length; i++) {
+        const btn = progressBtns[i];
+        /* Active state */
+        btn.addEventListener('click', function(){
+            let active = this.classList.contains('active');
+            /* Go into each button in order to see if there's any of them with the 'active' class already */
+            for(let btn2 of progressBtns){
+                /* If so, remove it */
+                btn2.classList.contains('active') ? btn2.classList.remove('active') : '';
+            }
+            /* And then toggle your active class on the actual clicked button */
+        /*  if(active){
+                this.classList.remove('active');
+            }else{
+                this.classList.add('active');
+            } 
+        */
+            this.classList.add('active');
+        });
+
+        btn.addEventListener('click', function(){
+            /* Move the slider to the btn clicked */
+            moveNCarousel(i);
+        });
+        
+    }
+
+
 }
 createProgressBar();
 
 
 /* On every window's resizing, it should calculate again. */
 window.onresize = () => {
-    while (progressBar.firstChild) {
-    progressBar.removeChild(progressBar.firstChild);
+    if(progressBar.firstChild){
+        while (progressBar.firstChild) {
+            progressBar.removeChild(progressBar.firstChild);
+        }
     }
+
     createProgressBar();
     console.log('resized')
 };
@@ -104,30 +136,4 @@ rightBtn.onclick = moveRight
 /* Left */
 leftBtn.onclick = moveLeft
 
-/* Event listeners for our progress buttons */
-for (let i = 0; i < progressBtns.length; i++) {
-    const btn = progressBtns[i];
-    /* Active state */
-    btn.addEventListener('click', function(){
-        let active = this.classList.contains('active');
-        /* Go into each button in order to see if there's any of them with the 'active' class already */
-        for(let btn2 of progressBtns){
-            /* If so, remove it */
-            btn2.classList.contains('active') ? btn2.classList.remove('active') : '';
-        }
-        /* And then toggle your active class on the actual clicked button */
-    /*  if(active){
-            this.classList.remove('active');
-        }else{
-            this.classList.add('active');
-        } 
-    */
-        this.classList.add('active');
-    });
 
-    btn.addEventListener('click', function(){
-        /* Move the slider to the btn clicked */
-        moveNCarousel(i);
-    });
-    
-}
